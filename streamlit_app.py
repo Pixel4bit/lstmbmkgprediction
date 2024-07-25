6c0ca0cd0d89edf334eddff596557d442ef2e37a
pianxd <81614152+Pixel4bit@users.noreply.github.com>
2024-07-25 21:08:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -434,19 +434,18 @@ if example_data:
     # UPDATES
     ## Interactive Plot with Plotly Go Scatter
     
-    # historical_data = climate_data['Tx'][2950:]
-    # combined_data = pd.concat([historical_data, pd.Series(data_hasil['Prediksi'], index=data_hasil.index)])
+    historical_data = climate_data['Tx'][2950:]
+    combined_data = pd.concat([historical_data, pd.Series(data_hasil['Prediksi'], index=data_hasil.index)])
     
-    # fig = go.Figure()
-    # fig.add_trace(go.Scatter(x=combined_data.index[:1099], y=combined_data[:1099],
-                             # mode='lines', line=dict(color='#134B70'), name='Data Historis'))
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=combined_data.index[:1099], y=combined_data[:1099],
+                             mode='lines', line=dict(color='#134B70'), name='Data Historis'))
     
-    # fig.add_trace(go.Scatter(x=combined_data.index[1099:], y=combined_data[1099:],
-                             # mode='lines', line=dict(color='red'), name='Prediksi'))
+    fig.add_trace(go.Scatter(x=combined_data.index[1099:], y=combined_data[1099:],
+                             mode='lines', line=dict(color='red'), name='Prediksi'))
     
-    # fig.update_traces(line=dict(width=2))  # Customize line width
-    # fig.update_layout(yaxis_title='Suhu', xaxis_title='Tanggal', height=600, title="Hasil Prediksi 1 Tahun Ke Masa Depan", title_x=0.5)
-    # fig.show()
+    fig.update_traces(line=dict(width=2))  # Customize line width
+    fig.update_layout(yaxis_title='Suhu', xaxis_title='Tanggal', height=600, title="Hasil Prediksi 1 Tahun Ke Masa Depan", title_x=0.5)
 
     prediction_col = st.columns((2, 0.2, 3))
     
@@ -458,7 +457,8 @@ if example_data:
     # Display scatter plot of actual vs predicted values
     with prediction_col[2]:
         st.markdown('Visualisasi')
-        st.pyplot(plt, use_container_width=True)
+        fig.show()
+        # st.pyplot(plt, use_container_width=True)
         # st.pyplot(fig, use_container_width=True)
 
     
